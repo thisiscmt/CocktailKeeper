0384477c55841e103a60dc0cf143f6341b16ef2a
Chris Tybur <thisiscmt@gmail.com>
2023-06-04 22:03:37 -0700

    Removed tests

--- src/pages/ErrorPage/ErrorPage.test.js
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react';
-
-import ErrorPage from './ErrorPage';
-
-test('Error page', () => {
-    render(<ErrorPage />);
-
-    expect(screen.getByTestId('ErrorMainContainer')).toBeInTheDocument();
-});
--- src/pages/Home/Home.test.js
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react';
-
-import Home from './Home';
-
-test('Home page', () => {
-    render(<Home />);
-
-    expect(screen.getByTestId('HomeMainContainer')).toBeInTheDocument();
-});
